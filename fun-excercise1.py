275f774f7726fbd408ab72331d25dd9e7d04da9c
vikram-dhamodaran <vikram.dhamodaran@gmail.com>
2023-12-31 16:31:27 +0530

    FuncExcercise updated

--- fun-excercise1.py
+++ fun-excercise1.py
@@ -5,13 +5,25 @@ Created on Sun Dec 31 15:54:02 2023
 @author: vikra
 """
 
-def cal_area(base, height):
-    area = (1/2)*base*height
+def cal_area(prm1, prm2, stype="Triangle"):
+    if stype == "rectangle":
+        area = prm1*prm2
+    else:
+        area = (1/2)*prm1*prm2
     return area
 
 base = 2
 height = 5
+stype = "rectangle"
+area_of_shape = cal_area(base, height, stype)
+print("area of Rectangle: ", area_of_shape)
 
-area_triangle = cal_area(base, height)
+base = 3
+height = 10
 
-print("area of Triange: ", area_triangle)
+area_of_shape = cal_area(base, height, stype)
+print("area of Triangle: ", area_of_shape)
+
+area_of_shape = cal_area(base, height)
+print("area of Triangle default shape: ", area_of_shape)
+    
